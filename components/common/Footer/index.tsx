8da5b30d4afdb9df5adfd1883ee53b9e03602469
Damir Modyarov <otomir@yandex.ru>
2022-07-04 20:31:59 +0300

    Fix copyright message

--- components/common/Footer/index.tsx
+++ components/common/Footer/index.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className="flex justify-center items-center p-4 border-t border-solid border-gray-dark text-gray h-min">
             <p className="text-sm">
-                Copyright © {new Date().getFullYear()} <a href="https://otomir23.me" className="text-gray-light" target="_blank" rel="noreferrer">otomir23</a>. Все права защищены.
+                Copyright © {new Date().getFullYear()} <a href="https://github.com/EsidaTeam" className="text-gray-light" target="_blank" rel="noreferrer">EsidaTeam</a>. Все права защищены.
             </p>
         </footer>
     );
