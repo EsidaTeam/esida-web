53ad99b0ecf850349a8f72287ae33336ee52bb27
Damir Modyarov <otomir@yandex.ru>
2022-07-05 17:54:30 +0300

    Remove UI lib demo page

---
+++ pages/komaru.tsx
@@ -0,0 +1,12 @@
+import {NextPage} from "next";
+import {Layout} from "../components/common";
+
+const Komaru: NextPage = () => {
+    return (
+        <Layout title="жеесть крылышки календула">
+            <p className="w-full text-center">меня сукожило!!!!!!! 📡📡📡 молекула!!! тапочки</p>
+        </Layout>
+    );
+}
+
+export default Komaru;
